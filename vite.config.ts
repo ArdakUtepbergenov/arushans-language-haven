6d92f819ecefc9c7cd2eee03b391bfd5010d968e
ArdakUtepbergenov <98506975+ArdakUtepbergenov@users.noreply.github.com>
2025-10-18 19:12:46 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  base: "/arushans-language-haven/",
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
